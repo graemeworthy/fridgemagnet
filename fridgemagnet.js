d933a71a46d2b8709f4df48d1ca81e22447f2eee
Graeme Worthy <graemeworthy@gmail.com>
2011-03-17 14:57:25 -0700

    initial commit

---
+++ fridgemagnet.js
@@ -0,0 +1,48 @@
+Fridgemagnet = {
+	supported: function() {
+		try {
+			return 'localStorage' in window && window['localStorage'] !== null;
+		} catch(e) {
+			return false;
+		}
+	},
+	storage_check: function(url) {
+		return !!localStorage[url];
+	},
+	fetch: function(url) {
+		var mycopy = nil;
+		if (Fridgemagnet.supported() && Fridgemagnet.storage_check(url)) {
+			mycopy_data = localStorage[url];
+			mycopy = eval(mycopy_data);
+			setTimeout(Fridgemagnet.normal_fetch, 2000, url, false); // background update
+		} else {
+			mycopy = Fridgemagnet.normal_fetch(url, true); // blocking fetch 
+		}
+		return mycopy;
+	},
+	save: function(url, data) {
+	  try {
+	    localStorage[url] = data;
+	  } catch (e) {
+	    return false;
+	  }	
+	 return true;
+	},
+	normal_fetch: function(url, blocking) {
+		var is_blocking = !!blocking;
+		var myurl = url;
+		var mycopy;
+		$.ajax({
+			url: myurl,
+			async: (is_blocking ? false : true),
+			success: function(data) {
+				mycopy = eval(data);
+				Fridgemagnet.save(myurl, data);
+			},
+			error: function(data) {
+				
+			}
+		});
+		return mycopy;
+	}
+};
